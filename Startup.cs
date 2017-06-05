c54b488b18a293cdfb1da0826c07bd800ad539b8
Schalk Bower <schalkbower@Schalks-MacBook-Air.local>
2017-06-05 20:43:11 +1200

    first commit

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace ausnimbus
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Test");
             });
         }
     }
